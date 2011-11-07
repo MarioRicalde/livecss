5fc036cb05d44f4783c3384ace811f2aee3fc0a5
Skurikhin Aleksandr <ahtungster@gmail.com>
2011-11-07 06:05:51 +0400

    Also very draft, with generaing syntax and color files support

--- css-colors.py
+++ css-colors.py
@@ -1,3 +1,5 @@
+from shutil import copyfile
+from os.path import join
 import sublime
 import sublime_plugin
 
@@ -18,3 +20,69 @@ class CssColorsCommand(sublime_plugin.TextCommand):
         colors_by_rgb = self.view.find_by_selector("constant.other.color.rgb-value.css")
         colors_by_hex = self.view.find_by_selector("support.constant.color.w3c-standard-color-name.css")
         print [self.view.substr(color) for color in colors_by_names + colors_by_hex]
+
+# %s1 color code without leading # char, %s2 color code
+theme_template = """<dict>
+                    <key>name</key>
+                    <string>css-colorize</string>
+                    <key>scope</key>
+                    <string>%s</string>
+                    <key>settings</key>
+                    <dict>
+                        <key>foreground</key>
+                        <string>%s</string>
+                    </dict>
+        </dict>"""
+
+# %s = syntax rule for one color
+synax_genral = """
+    <key>facepalm-values</key>
+        <dict>
+            <key>patterns</key>
+            %s
+        </dict>
+"""
+# %s = color hex code without # prefix
+syntax_color_template = """
+    <dict>
+        <key>captures</key>
+        <dict>
+            <key>1</key>
+            <dict>
+                <key>name</key>
+                <string>punctuation.definition.constant.css</string>
+            </dict>
+        </dict>
+        <key>match</key>
+        <string>(#)(%s)\b</string>
+        <key>name</key>
+        <string>%s.css-colorize.css</string>
+    </dict>
+"""
+sytax_rule_template = synax_genral % syntax_color_template % color
+
+
+def generate_syntax(color_codes):
+    """Generate new css syntax with `color_code`,
+    #Args:
+    color_codes: list of str
+    """
+
+    rules = [syntax_color_template % color_code.split('#')[-1]  for color_code in color_codes]
+    with open('"Packages/CSS/CSS.tmLanguage') as syn_file:
+        syn_file_content = syn_file.read()
+    css_dir_path = join(self.view.packages_path(), "CSS")
+
+    # copy css synatax file
+    copyfile(join(css_dir_path, "CSS.tmLanguage"), join(css_dir_path, "CSS-colorized.tmLanguage"))
+    # write new color rules to newly crated syntax file
+    with open join(css_dir_path, "CSS-colorized.tmLanguage") as syntax_f:
+        pass
+
+
+def generate_theme(color_code):
+    rules = theme_template % (color_code.split('#')[-1], color_code)
+    # check if we can apply colorsheme just of updating current color file
+    # or we have to copy it like syn file
+    # code
+
