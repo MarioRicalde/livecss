f5566874bef329c4d41ff841a8d901ea583cc255
a-sk <ahtungster@gmail.com>
2012-02-04 04:49:59 +0400

    Add more documentation to helpers module

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,20 +1,44 @@
-escape = lambda s: "\'" + s + "\'"
+"""Defines some useful python objects
 
-make_eq = lambda x: lambda y: x == y
+author: Alexandr Skurihin [a.skurihin@gmail.com]
+last-modified: 04.02.2012
+"""
 
 
 class AvailabilityChecker(object):
-    def __init__(self, callbacks):
-        if not isinstance(callbacks, list) and \
-           not isinstance(callbacks, tuple):
-            callbacks = [callbacks]
+    """Makes 'in' checks easily.
+    Apply given @comparators to checking object,
+    if item in @comparators is not callable, checks if comparing object
+    is equal to this item in @comparators
 
-        self._callbacks = []
-        for cb in callbacks:
+
+    >>>checker = AvailabilityChecker("checkme")
+    >>>"checkme" in checker
+    True
+    >>>checker = AvailabilityChecker(lambda s: s.startswith('_'))
+    >>>"_checkme" in checker
+    True
+
+    """
+    def __init__(self, comparators):
+        """
+        @param {anytype} comparators
+        """
+        if not isinstance(comparators, list) and \
+           not isinstance(comparators, tuple):
+            comparators = [comparators]
+
+        self._comparators = []
+        for cb in comparators:
             if not callable(cb):
-                self._callbacks.append(make_eq(cb))
+                self._comparators.append(make_eq(cb))
             else:
-                self._callbacks.append(cb)
+                self._comparators.append(cb)
 
     def __contains__(self, attr):
-        return any(cb(attr) for cb in self._callbacks)
+        return any(cb(attr) for cb in self._comparators)
+
+
+escape = lambda s: "\'" + s + "\'"
+
+make_eq = lambda x: lambda y: x == y
